afbc012682b1530df8c3fa114fc76a171f9b6e9f
Arindam Dey <deyarindam475@gmail.com>
2021-06-21 19:40:06 +0530

    Create CountDistinctElements.py

---
+++ CountDistinctElements.py
@@ -0,0 +1,23 @@
+#Without using set:
+
+def cDistinctFinder(l):
+    res = 1
+    
+    for i in range(1,len(l)):
+        if l[i] not in l[0:i]:
+            res = res+1
+    
+    return res
+  
+l = [10,20,10,30,30,20]
+print(cDistinctFinder(l))
+
+
+#using Set:---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def cDistinctFinder2(l):
+  return len(set(l))
+
+l = [10,20,10,30,30,20]
+print(cDistinctFinder2(l))
+  
