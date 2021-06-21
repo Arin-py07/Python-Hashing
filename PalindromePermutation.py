5f2d8e5fb6a2b5e71b162518aa9c8995f792b9e0
Arindam Dey <deyarindam475@gmail.com>
2021-06-21 20:38:47 +0530

    Create PalindromePermutation.py

---
+++ PalindromePermutation.py
@@ -0,0 +1,25 @@
+Input : "geegsgeeks"
+Output : True
+  
+Input : "abaaac"
+Output : False
+  
+Solution:
+  
+from Collection import Counter #Counter is a subclass of Dictionary.
+
+def isPalPer(s):
+  cnt = Counter(s)
+  odd = 0
+  
+  for freq in cnt.values():
+    
+    if freq%2 != 0:
+      odd += 1
+      
+      if odd > 1 :
+        return False
+      
+  return True
+        
+  
